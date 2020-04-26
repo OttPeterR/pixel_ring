0eaea3d0a5d88bd1a5f39a67fb017644bd428821
OttPeterR <ott.peter.r@gmail.com>
2020-04-26 20:38:33 +0100

    theme selection bug fixed

--- pixel_ring/demo.py
+++ pixel_ring/demo.py
@@ -1,13 +1,13 @@
 import time
 import argparse
-from pixel_ring import PixelRing
+from apa102_pixel_ring import PixelRing
 
 parser = argparse.ArgumentParser(description='Light ring demo')
-parser.add_argument('theme_name', default="google", type=str, nargs=1, help='theme: [echo, google]')
+parser.add_argument('--theme', type=str, required=False, default='echo', choices=['echo', 'google'])
 
-theme = parser.parse_args().theme_name[0]
-
-pixel_ring = PixelRing()
+theme = parser.parse_args().theme
+pixel_ring = PixelRing(pattern=theme)
+pixel_ring.set_brightness(10)
 while True:
     try:
         print("wakeup")
