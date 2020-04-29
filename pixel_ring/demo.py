4844a07c60f315b1c45eb5889ec6bc7bb04be262
OttPeterR <ott.peter.r@gmail.com>
2020-04-29 05:04:07 +0100

    changed times to make viewing different actions easier

--- pixel_ring/demo.py
+++ pixel_ring/demo.py
@@ -3,28 +3,29 @@ import argparse
 from apa102_pixel_ring import PixelRing
 
 parser = argparse.ArgumentParser(description='Light ring demo')
-parser.add_argument('--theme', type=str, required=False, default='echo', choices=['echo', 'google'])
+parser.add_argument('--theme', type=str, required=False, default='echo', choices=['echo', 'google', 'mytheme1', '1'])
 
 theme = parser.parse_args().theme
 pixel_ring = PixelRing(pattern=theme)
 pixel_ring.set_brightness(10)
+t = 5
 while True:
     try:
         print("wakeup")
         pixel_ring.wakeup()
-        time.sleep(3)
+        time.sleep(t)
         print("thinking")
         pixel_ring.think()
-        time.sleep(3)
+        time.sleep(t)
         print("speaking")
         pixel_ring.speak()
-        time.sleep(6)
+        time.sleep(t)
         print("listening")
         pixel_ring.listen()
-        time.sleep(3)
+        time.sleep(t)
         print("off")
         pixel_ring.off()
-        time.sleep(3)
+        time.sleep(1)
     except KeyboardInterrupt:
         break
 
